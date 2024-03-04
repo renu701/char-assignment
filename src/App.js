e6aa1d45360d88a3db423c9091877c792966469f
Renu <apple@Apples-MacBook-Pro.local>
2024-03-04 10:30:25 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () =>{
+    return 'test'
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLayout />);
